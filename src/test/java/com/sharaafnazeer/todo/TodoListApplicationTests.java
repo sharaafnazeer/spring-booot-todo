77b635974dfc77dcd1f3f8065d4685e136b01703
sharaafnazeer <ahamedsharaaf@gmail.com>
2020-05-06 22:12:12 +0530

    Initial Commit

---
+++ src/test/java/com/sharaafnazeer/todo/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharaafnazeer.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
